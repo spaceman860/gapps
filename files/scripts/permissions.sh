7353609c0dee68160c93925d4811f35fcd6a4e52
osm0sis <osm0sis@outlook.com>
2016-01-19 15:33:57 -0600

    gapps: switch to set_metadata_recursive
    * also remove unneeded cert files
    
    Change-Id: Iec076d6ab362ba0c7d212c6a54f5d94a9534bb16

--- files/scripts/permissions.sh
+++ files/scripts/permissions.sh
@@ -13,39 +13,34 @@
 #    GNU General Public License for more details.
 
 # Functions
-set_perm_recursive() {
-  dirs=$(echo $* | awk '{ print substr($0, index($0,$5)) }');
-  for i in $dirs; do
-    chown -R $1.$2 $i; chown -R $1:$2 $i;
-    find "$i" -type d -exec chmod $3 {} +;
-    find "$i" -type f -exec chmod $4 {} +;
+set_metadata_recursive() {
+  dir="$1";
+  shift;
+  until [ ! "$2" ]; do
+    case $1 in
+      uid) chown -R $2 $dir;;
+      gid) chown -R :$2 $dir;;
+      dmode) find "$dir" -type d -exec chmod $2 {} +;;
+      fmode) find "$dir" -type f -exec chmod $2 {} +;;
+      capabilities) ;;
+      selabel)
+        for i in /system/bin/toybox /system/toolbox /system/bin/toolbox; do
+          find "$dir" -exec LD_LIBRARY_PATH=/system/lib $i chcon -h $2 {} +;
+          find "$dir" -exec LD_LIBRARY_PATH=/system/lib $i chcon $2 {} +;
+        done;
+        find "$dir" -exec chcon -h $2 '{}' +;
+        find "$dir" -exec chcon $2 '{}' +;
+      ;;
+      *) ;;
+    esac;
+    shift 2;
   done;
 }
-
-ch_con_recursive() {
-  dirs=$(echo $* | awk '{ print substr($0, index($0,$3)) }');
-  for i in $dirs; do
-    for j in /system/bin/toybox /system/toolbox /system/bin/toolbox; do
-      find "$i" -type d -exec LD_LIBRARY_PATH=/system/lib $j chcon -h u:object_r:system_file:s0 {} +;
-      find "$i" -type f -exec LD_LIBRARY_PATH=/system/lib $j chcon -h u:object_r:system_file:s0 {} +;
-      find "$i" -type d -exec LD_LIBRARY_PATH=/system/lib $j chcon u:object_r:system_file:s0 {} +;
-      find "$i" -type f -exec LD_LIBRARY_PATH=/system/lib $j chcon u:object_r:system_file:s0 {} +;
-    done;
-    find "$i" -type d -exec chcon -h u:object_r:system_file:s0 '{}' +;
-    find "$i" -type f -exec chcon -h u:object_r:system_file:s0 '{}' +;
-    find "$i" -type d -exec chcon u:object_r:system_file:s0 '{}' +;
-    find "$i" -type f -exec chcon u:object_r:system_file:s0 '{}' +;
-  done;
-}
-
-# Set permissions
-set_perm_recursive 0 0 755 755 "/system/addon.d";
-set_perm_recursive 0 0 755 644 "/system/app" "/system/etc/permissions" "/system/etc/preferred-apps" "/system/etc/sysconfig" "/system/framework" "/system/lib" "/system/lib64" "/system/priv-app" "/system/usr/srec" "/system/vendor/lib" "/system/vendor/lib64" "/system/vendor/pittpatt";
 
 # Change pittpatt folders to root:shell per Google Factory Settings
 find "/system/vendor/pittpatt" -type d -exec chown 0.2000 '{}' \;
 
-# Set selinux contexts
-ch_con_recursive "/system/addon.d" "/system/app" "/system/etc/permissions" "/system/etc/preferred-apps" "/system/etc/sysconfig" "/system/framework" "/system/lib" "/system/lib64" "/system/priv-app" "/system/usr/srec" "/system/vendor/lib" "/system/vendor/lib64" "/system/vendor/pittpatt";
+# Set metadata
+set_metadata_recursive "/system/addon.d" "/system/app" "/system/etc/permissions" "/system/etc/preferred-apps" "/system/etc/sysconfig" "/system/framework" "/system/lib" "/system/lib64" "/system/priv-app" "/system/usr/srec" "/system/vendor/lib" "/system/vendor/lib64" "/system/vendor/pittpatt";
 
 exit 0
